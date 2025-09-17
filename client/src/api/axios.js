3be8cd94420a0765bd58f49205357cb7e031e0b2
Gideon <kwarteon08@gmail.com>
2025-09-17 14:11:25 +0000

    new link

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://q0smnp61-5000.uks1.devtunnels.ms";
 
 const api = axios.create({
   baseURL: API_URL,
